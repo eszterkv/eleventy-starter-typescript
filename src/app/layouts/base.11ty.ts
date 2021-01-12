f56db320d63fd56c2d49d3ded7502cb527cf7efa
eszter <ekov@protonmail.ch>
2021-01-12 13:42:59 +0000

    fix SEO issues & add deploy to vercel/netlify buttons

--- src/app/layouts/base.11ty.ts
+++ src/app/layouts/base.11ty.ts
@@ -3,16 +3,25 @@ import navigation from '../components/navigation'
 interface PageProps {
   title: string;
   content: string;
+  lang: string;
+  description: string;
 }
 
-module.exports = ({ title, content }: PageProps) => `
+const defaultDescription = 'TypeScript starter for Eleventy'
+
+module.exports = ({
+  title,
+  content,
+  lang = 'en',
+  description = defaultDescription,
+}: PageProps) => `
   <!doctype html>
-  <html>
+  <html lang="${lang}">
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="${description}">
       <title>${title || 'Hello world'}</title>
-      <link rel="stylesheet" href="/main.bundle.css" />
     </head>
     <body>
       <header>
